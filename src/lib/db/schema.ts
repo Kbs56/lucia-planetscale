e4dd567727440fdf1566f0b16658b1971c3ffb6a
Kenny Sheldon <kenny.sheldon99@gmail.com>
2024-02-03 17:05:56 -0600

    Adding users and sessions tables

--- src/lib/db/schema.ts
+++ src/lib/db/schema.ts
@@ -1,7 +1,19 @@
-import { mysqlTable, varchar } from 'drizzle-orm/mysql-core';
+import { datetime, mysqlTable, varchar } from 'drizzle-orm/mysql-core';
 
 export const contact = mysqlTable('contact', {
 	firstName: varchar('first_name', { length: 100 }).notNull(),
 	lastName: varchar('last_name', { length: 100 }).notNull(),
 	email: varchar('email', { length: 255 }).primaryKey()
+});
+
+export const user = mysqlTable('user', {
+	id: varchar('id', { length: 255 }).primaryKey()
+});
+
+export const session = mysqlTable('session', {
+	id: varchar('id', { length: 255 }).primaryKey(),
+	user_id: varchar('user_id', { length: 255 })
+		.notNull()
+		.references(() => user.id),
+	expires_at: datetime('expires_at').notNull()
 });
