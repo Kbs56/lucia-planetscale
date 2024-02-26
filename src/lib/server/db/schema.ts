08fdeb794acf3bf06e264d6e050da0af6de6db94
Kenny Sheldon <kenny.sheldon99@gmail.com>
2024-02-25 21:10:19 -0600

    Update db schema

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -2,6 +2,7 @@ import { datetime, mysqlTable, varchar } from 'drizzle-orm/mysql-core';
 
 export const user = mysqlTable('user', {
 	id: varchar('id', { length: 255 }).primaryKey().notNull(),
+	username: varchar('username', { length: 255 }).unique(),
 	email: varchar('email', { length: 255 }).notNull().unique(),
 	password: varchar('password', { length: 255 }).notNull()
 });
