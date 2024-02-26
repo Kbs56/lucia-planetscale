e9ca38d84bf2a926001603895607dcb5b7cc5ed0
Kenny Sheldon <kenny.sheldon99@gmail.com>
2024-02-25 20:47:38 -0600

    Implement Logout

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,5 +1,6 @@
-import { redirect } from '@sveltejs/kit';
+import { redirect, fail, type Actions } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
+import { lucia } from '$lib/server/auth';
 
 export const load: PageServerLoad = async (event) => {
 	if (event.locals.user) {
@@ -8,5 +9,20 @@ export const load: PageServerLoad = async (event) => {
 		};
 	} else {
 		redirect(302, '/auth/register');
+	}
+};
+
+export const actions: Actions = {
+	default: async (event) => {
+		if (!event.locals.session) {
+			return fail(401);
+		}
+		await lucia.invalidateSession(event.locals.session.id);
+		const sessionCookie = lucia.createBlankSessionCookie();
+		event.cookies.set(sessionCookie.name, sessionCookie.value, {
+			path: '.',
+			...sessionCookie.attributes
+		});
+		redirect(302, '/auth/login');
 	}
 };
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,8 +4,11 @@
 
 <title>Home</title>
 
-<div class="flex h-screen items-center justify-center">
+<div class="flex h-screen flex-col items-center justify-center">
 	{#if data.user}
 		<h1 class="text-2xl text-slate-800">Hello {data.user.email}</h1>
+		<form method="POST">
+			<button>Sign Out</button>
+		</form>
 	{/if}
 </div>
